6e681cf3be895b4e1a0abd2359c7904ce027bad8
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-10-16 12:58:50 -0400

    Add Black linter for Python formatting

--- trophies/apps.py
+++ trophies/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TrophiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trophies'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "trophies"
