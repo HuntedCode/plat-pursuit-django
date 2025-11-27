97c469f3ad625a4f89a8de11d4079f4cfd170101
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-11-27 01:41:51 -0500

    Fixed logic bug in add_region method of Game model

--- trophies/management/commands/convert_regions.py
+++ trophies/management/commands/convert_regions.py
@@ -1,15 +1,13 @@
 from django.core.management.base import BaseCommand
 from trophies.models import Game
-from trophies.utils import NA_REGION_CODES, EU_REGION_CODES, JP_REGION_CODES, AS_REGION_CODES
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         games = Game.objects.all()
         for game in games:
             if game.region:
-                old_regions = game.region
-                game.region = []
+                distinct_region = set()
+                for r in game.region:
+                    distinct_region.add(r)
+                game.region = list(distinct_region)
                 game.save(update_fields=['region'])
-
-                for region in old_regions:
-                    game.add_region(region)
