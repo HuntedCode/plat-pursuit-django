f6c2a6fb636e3c5fbacb8dd0d8edd2826ab7d3b4
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-12-19 09:47:18 -0500

    Add DEBUG from .env (bug testing)

--- plat_pursuit/settings.py
+++ plat_pursuit/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
