9242aa0aee48f92788ac5dad35fe92bea57d6641
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-12-27 00:54:51 -0500

    Add storages dict to settings

--- plat_pursuit/settings.py
+++ plat_pursuit/settings.py
@@ -235,11 +235,6 @@ MEDIA_URL = "/media/"
 if DEBUG:
     MEDIA_ROOT = BASE_DIR / "media"
 else:
-    class PublicMediaStorage(S3Boto3Storage):
-        location = 'media'
-        default_acl = 'public-read'
-        file_overwrite = False
-
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
     AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
@@ -248,8 +243,17 @@ else:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'plat_pursuit.settings.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_DEFAULT_ACL = None
+
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                'location': 'media',
+            },
+        },
+    }
 
     print("Using AWS, values:")
     print('AWS_S3_ACCESS_KEY_ID', AWS_S3_ACCESS_KEY_ID)
