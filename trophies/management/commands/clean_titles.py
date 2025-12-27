2eda5b4ac3c739e451aa711091b23131bc9b3b5f
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-12-27 03:56:26 -0500

    Add batching for memory perf

--- trophies/management/commands/clean_titles.py
+++ trophies/management/commands/clean_titles.py
@@ -6,37 +6,56 @@ class Command(BaseCommand):
     help = 'Remove TM/® symbols from existing titles'
 
     def handle(self, *args, **options):
-        games = Game.objects.all()
+        updated_items = []
         updated_count = 0
-        for game in games:
+        for game in Game.objects.all().iterator(chunk_size=1000):
             original_title = game.title_name
             cleaned_title = re.sub(r'[™®]|(\bTM\b)|(\(R\))', '', original_title).strip()
             if cleaned_title != original_title:
                 game.title_name = cleaned_title
-                game.save(update_fields=['title_name'])
+                updated_items.append(game)
                 updated_count += 1
+
+                if len(updated_items) >= 1000:
+                    Game.objects.bulk_update(updated_items, ['title_name'])
+                    updated_items = []
+        if updated_items:
+            Game.objects.bulk_update(updated_items, ['title_name'])
+            updated_items = []
         self.stdout.write(f"Cleaned {updated_count} games successfully.")
 
-        concepts = Concept.objects.all()
+        updated_items = []
         updated_count = 0
-        for concept in concepts:
+        for concept in Concept.objects.all().iterator(chunk_size=1000):
             original_title = concept.unified_title
             cleaned_title = re.sub(r'[™®]|(\bTM\b)|(\(R\))', '', original_title).strip()
             if cleaned_title != original_title:
                 concept.unified_title = cleaned_title
-                concept.save(update_fields=['unified_title'])
+                updated_items.append(concept)
                 updated_count += 1
+
+                if len(updated_items) >= 1000:
+                    Concept.objects.bulk_update(updated_items, ['unified_title'])
+                    updated_items = []
+        if updated_items:
+            Concept.objects.bulk_update(updated_items, ['unified_title'])
         self.stdout.write(f"Cleaned {updated_count} concepts successfully.")
 
-        trophies = Trophy.objects.all()
+        updated_items = []
         updated_count = 0
-        for trophy in trophies:
+        for trophy in Trophy.objects.all().iterator(chunk_size=1000):
             original_title = trophy.trophy_name
             cleaned_title = re.sub(r'[™®]|(\bTM\b)|(\(R\))', '', original_title).strip()
             if cleaned_title != original_title:
                 trophy.trophy_name = cleaned_title
-                trophy.save(update_fields=['trophy_name'])
+                updated_items.append(trophy)
                 updated_count += 1
+
+                if len(updated_items) >= 1000:
+                    Trophy.objects.bulk_update(updated_items, ['trophy_name'])
+                    updated_items = []
+        if updated_items:
+            Trophy.objects.bulk_update(updated_items, ['trophy_name'])
         self.stdout.write(f"Cleaned {updated_count} trophies successfully.")
 
         self.stdout.write(self.style.SUCCESS('All titles cleaned successfully!'))
