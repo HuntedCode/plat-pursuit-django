60755e7187d394de735eafdcc5fefc5085c81670
Jeffrey Lowe <HuntedCode47@gmail.com>
2026-01-04 00:26:47 -0500

    Update logic for new discord roles

--- trophies/utils.py
+++ trophies/utils.py
@@ -171,14 +171,14 @@ def handle_badge(profile, badge, add_role_only=False):
         elif not badge_earned and user_badge_exists:
             UserBadge.objects.filter(profile=profile, badge=badge).delete()
             logger.info(f"Revoked badge {badge.effective_display_title} (tier: {badge.tier}) from {profile.display_psn_username}")
+
+        if badge_earned and badge.discord_role_id:
+            if profile.is_discord_verified and profile.discord_role_id:
+                notify_bot_role_earned(profile, badge.discord_role_id)
         
         if not add_role_only and badge_created and badge.discord_role_id:
             if profile.is_discord_verified and profile.discord_id:
                 notify_new_badge(profile, badge)
-                notify_bot_role_earned(profile, badge.discord_role_id)
-        elif add_role_only and badge.discord_role_id and user_badge_exists:
-            if profile.is_discord_verified and profile.discord_id:
-                notify_bot_role_earned(profile, badge.discord_role_id)
 
         return badge_created
 
